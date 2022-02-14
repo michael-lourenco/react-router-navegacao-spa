9ef3ef4dbe7394b386f10e706f959af88bf8614f
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-14 00:22:20 -0300

    Início do problema

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import React from 'react';
+import './assets/css/base/base.css';
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre';
 
 function App() {
+  const Router = () => {
+    const location = window.location.pathname;
+    
+    if (location === '/') {
+      return <Home />;
+    }
+    if (location === '/sobre') {
+      return <Sobre />;
+    } else {
+      return <Home />;
+    }
+  }
   return (
   <>
-
+    { Router() }
   </>
   )
 }
