3480883dacac9d58eafb2130878a0f05dd19efb6
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-15 21:06:24 -0300

    Exibindo os posts

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:5001",
+    baseURL: "http://localhost:5000",
 });
 
-export const busca = async () => {
+export const busca = async (url, setDado) => {
     const resposta =  await api.get(url);
     setDado(resposta.data);
 }
