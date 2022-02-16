96f3de7f6d6a7ff168d883dc5043f692741d7171
Michael Lourenco <michael.lourenco@pixter.com.br>
2022-02-15 22:19:35 -0300

    Post inexistente

--- src/paginas/Post.jsx
+++ src/paginas/Post.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { busca } from '../api/api';
 import '../assets/css/post.css';
 
 const Post = () => {
+    let history = useHistory();
     const { id } = useParams();
     const [post, setPost] = useState({});
 
     useEffect(() => {
         busca(`/posts/${id}`, setPost)
+        .catch(() => {
+                history.push('/404');
+            }
+        )
     },[id])
 
     return (
